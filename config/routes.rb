4e334770a56924c848f2fc8f06189d110f7ec27b
yokoko1164 <yokoko1164@gmail.com>
2015-11-28 23:01:57 +0900

    ２つ目のコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root to: "top#index"
+  resources :requests
 
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
     controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
