0dc73241f0c726aa62d324809763ccc7c07a7786
yokoko1164 <yokoko1164@gmail.com>
2015-11-28 23:50:41 +0900

    コミット

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,17 @@
 class RequestsController < ApplicationController
   def index
   end
+
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new(request_params)
+  end
+
+  private
+    def request_params
+       params.require(:request).permit(:name, :datetime, :comment)
+    end
 end
